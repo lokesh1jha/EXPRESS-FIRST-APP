8a0b96070b1bc2edeb769648eb3cfbe87c3b6423
LOKESH KUMAR JHA <83881305+lokesh1jha@users.noreply.github.com>
2022-06-28 20:29:16 +0530

    app.use -> app.post
    app.use gets executed for both request POST & GET
    app.get limits it to GET Request
    Similarly app.post limits to POST Request

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const http = require('http');
 
+//npm install --save body-parser
+const bodyParser = require('body-parser');
+
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allow  the request to continue to the next middleware in line
+//extend non default also
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST">' +
+    '<input type="text" name="product" placeholder="Product Name">'+
+    '<input type="text" name="size" placeholder="Product Size">'+
+    '<button type="submit">Add Product</button></form>');
 })
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<html><h1>Hi This Is Express</h1></html>')
-})
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);
